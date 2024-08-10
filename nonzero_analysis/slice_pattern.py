f3eba6939f1392cbfa790854347b4e1bbc2c3959
Vivek Bharadwaj <vivek_bharadwaj@berkeley.edu>
2024-08-09 23:21:41 -0700

    Added a function to fill the diagonal.

--- nonzero_analysis/slice_pattern.py
+++ nonzero_analysis/slice_pattern.py
@@ -1,8 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def fill_diagonal(slice, offset, direction):
+    assert(direction == 1 or direction == -1)
+    rows, cols = slice.shape
+    center = rows // 2 + offset, cols // 2
+
+    cur = center
+    while 0 <= cur[0] < rows and 0 <= cur[1] < cols:
+        slice[cur[0], cur[1]] = 1.0
+        cur = cur[0] + 1, cur[1] + direction
+
+    cur = center
+    while 0 <= cur[0] < rows and 0 <= cur[1] < cols:
+        slice[cur[0], cur[1]] = 1.0
+        cur = cur[0] - 1, cur[1] - direction 
+
+
+
 def create_nonzero_pattern(L1, L2, L3, slice_dim):
-    tensor = np.zeros(2 * L1 + 1, 2 * L2 + 1, 2 * L3 + 1)
+    tensor = np.zeros((2 * L1 + 1, 2 * L2 + 1, 2 * L3 + 1))
 
     for slice_idx in range(tensor.shape[slice_dim]):
         if slice_dim == 0:
@@ -12,7 +29,7 @@ def create_nonzero_pattern(L1, L2, L3, slice_dim):
         else:
             slice = tensor[:, :, slice_idx]
 
-        slice += np.diag(1.0, 0)
+        fill_diagonal(slice, 5, -1)
 
     return tensor
 
@@ -20,7 +37,7 @@ if __name__=='__main__':
     slice_dim = 0
     slice_idx = 2
 
-    pattern_tensor = create_nonzero_pattern(5, 5, 5, slice_dim)
+    pattern_tensor = create_nonzero_pattern(L1=5, L2=5, L3=2, slice_dim=slice_dim)
 
     if slice_dim == 0:
         slice = pattern_tensor[slice_idx, :, :]
@@ -30,4 +47,4 @@ if __name__=='__main__':
         slice = pattern_tensor[:, :, slice_idx]
 
     plt.imshow(slice)
-
+    plt.show()
