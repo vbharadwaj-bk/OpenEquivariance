f4928f76caf694bc39a135617f90700de8a810f3
Vivek Bharadwaj <67718556+vbharadwaj-bk@users.noreply.github.com>
2024-09-09 14:11:57 -0700

    Added logging to quickly view throughput and see correctness check failures. (#10)
    * Added script to download MathDX.
    
    * Updated CMakeLists.txt to find cuBLASDx.
    
    * Added a representation class.
    
    * Progress creating a dedicated representation class.
    
    * Driver works with Representation class.
    
    * Commented out MathDX for now.
    
    * Began work on shuffle-reduce tensor product.
    
    * More progress on a shuffle reduce TP.
    
    * Calculated broadcast and reduction lanes, ready to prototype.
    
    * Wrote Python prototype of the warp shuffle tensor product.
    
    * Debugging shuffle reduce prototype.
    
    * More progress debugging, but errors remain.
    
    * Prototype works using masking.
    
    * Added C++ layer for the shuffle tensor product.
    
    * We have a compiling implementation.
    
    * CUDA kernel in progress.
    
    * Finished prototype of CUDA kernel.
    
    * It works for batch size 1!
    
    * Ready to benchmark the shuffle-reduce TP.
    
    * Benchmark complete.
    
    * Added fancy printout of GFLOPs rate.
    
    * Changed location of reshape.
    
    * Verified passing for shuffle reduce product.
    
    * Added logging.

---
+++ src/benchmark/logging_utils.py
@@ -0,0 +1,21 @@
+import logging
+logger = logging.getLogger("ETP")
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+def getLogger():
+    return logger
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
