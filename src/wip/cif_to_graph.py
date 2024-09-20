93ad3a8f6613119615abec8ab7b947602ea769ed
Vivek Bharadwaj <vivek_bharadwaj@berkeley.edu>
2024-09-19 22:11:48 -0700

    Created multiple graphs with cutoff radii.

--- src/wip/cif_to_graph.py
+++ src/wip/cif_to_graph.py
@@ -1,8 +1,9 @@
 import pickle
 import numpy as np
 from sklearn.neighbors import radius_neighbors_graph
+from scipy.io import mmwrite
 
-def cif_to_molecular_graph(cif_file, cp, radius=3.5):
+def cif_to_molecular_graph(cif_file, cp, radii):
     with open(f'../../data/cif_files/{cif_file}', 'r') as f:
         print("Started reading file...")
         lines = f.readlines()
@@ -16,11 +17,24 @@ def cif_to_molecular_graph(cif_file, cp, radius=3.5):
 
         coords = np.array(coords)
 
-        print("Starting radius neighbors calculation...")
-        A = radius_neighbors_graph(coords, radius, mode='connectivity',
-                           include_self=False) 
-        print(f"Finished radius neighbors calculation, found {A.nnz} nonzeros.") 
+        for radius in radii:
+            print(f"Starting radius neighbors calculation, r={radius}")
+            A = radius_neighbors_graph(coords, radius, mode='connectivity',
+                            include_self=False) 
+            print(f"Finished radius neighbors calculation, found {A.nnz} nonzeros.") 
+    
+            # mmwrite(f'../../data/molecular_structures/{cif_file.split(".")[0]}.mtx', A)
+
+            coo_mat = A.tocoo()
+            result = {
+                'row': coo_mat.row,
+                'col': coo_mat.col,
+                'coords': coords
+            }
+
+            with open(f'../../data/molecular_structures/{cif_file.split(".")[0]}_radius{radius}.pickle', 'wb') as handle:
+                pickle.dump(result, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 if __name__=='__main__':
-    cif_to_molecular_graph('hiv_capsid.cif', (10, 11, 12), radius=3.5)  
+    cif_to_molecular_graph('hiv_capsid.cif', (10, 11, 12), radii=[2.0, 2.5, 3.0, 3.5])  
