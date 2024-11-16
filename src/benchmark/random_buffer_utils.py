7c0719a9f7fc2c99c4c1ee781bb76f4f8b6ded05
Austin Glover <austin_glover@berkeley.com>
2024-11-16 14:03:49 -0800

    rename

--- src/benchmark/random_buffer_utils.py
+++ src/benchmark/random_buffer_utils.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from src.implementations.e3nn_lite import TPProblem
 
-def get_random_supplies_forward(tpp : TPProblem, batch_size : int,  prng_seed : int) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+def get_random_buffers_forward(tpp : TPProblem, batch_size : int,  prng_seed : int) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     """
     Return properly sized numpy arrays needed to execute a tensor product in the forward direction
     Supports shared vs non-shared weights
@@ -13,14 +13,14 @@ def get_random_supplies_forward(tpp : TPProblem, batch_size : int,  prng_seed : 
     in1 = np.array(rng.uniform(size=(batch_size, tpp.irreps_in1.dim)), dtype=np.float32) 
     in2 = np.array(rng.uniform(size=(batch_size, tpp.irreps_in2.dim)), dtype=np.float32)
 
-    num_weights = tpp.weight_numel if tpp.shared_weights else tpp.weight_numel * batch_size
-    weights = np.array(rng.uniform(size=(num_weights)), dtype=np.float32)
+    weights_size = tuple([tpp.weight_numel]) if tpp.shared_weights else tuple([batch_size, tpp.weight_numel]) 
+    weights = np.array(rng.uniform(size=weights_size), dtype=np.float32)
 
-    out = np.zeros(shape=(batch_size * tpp.irreps_out.dim), dtype=np.float32)
+    out = np.zeros(shape=(batch_size, tpp.irreps_out.dim), dtype=np.float32)
 
     return in1, in2, weights, out 
 
-def get_random_supplies_backward(tpp : TPProblem, batch_size : int, prng_seed : int) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+def get_random_buffers_backward(tpp : TPProblem, batch_size : int, prng_seed : int) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     """
     Return properly sized numpy arrays needed to execute a tensor product in the backward direction
     Supports shared vs non-shared weights
@@ -32,8 +32,8 @@ def get_random_supplies_backward(tpp : TPProblem, batch_size : int, prng_seed : 
     in2 = np.array(rng.uniform(size=(batch_size, tpp.irreps_in2.dim)), dtype=np.float32)
     out_grad = np.array(rng.uniform(size=(batch_size, tpp.irreps_out.dim)), dtype=np.float32)
 
-    num_weights = tpp.weight_numel if tpp.shared_weights else tpp.weight_numel * batch_size
-    weights = np.array(rng.uniform(size=(num_weights)), dtype=np.float32)
+    weights_size = tuple(tpp.weight_numel) if tpp.shared_weights else tuple(batch_size, tpp.weight_numel) 
+    weights = np.array(rng.uniform(size=weights_size), dtype=np.float32)
 
     weights_grad = np.zeros_like(weights)
     in1_grad = np.zeros_like(in1)
