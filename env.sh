b33dc8c1a9bcf4c77869ce3062736185c4f72ef9
Austin Glover <austin_glover@berkeley.com>
2024-11-19 20:15:39 -0800

    move these to a separate file

--- env.sh
+++ env.sh
@@ -3,8 +3,6 @@ conda activate equivariant_spmm_env
 
 module load cudatoolkit
 
-export CMAKE_PREFIX_PATH=${CUDA_HOME}/../../math_libs:${CMAKE_PREFIX_PATH}
-export CPATH=${CUDA_HOME}/../../math_libs/include:${CPATH}
 
 # dcgmi profile --pause
 # ncu -c 2 -o report.ncu-rep python driver.py
