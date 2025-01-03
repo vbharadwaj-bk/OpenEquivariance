229c664d71b19c9d82fcf6283b8e2267f96cb5dd
Vivek Bharadwaj <vivek_bharadwaj@berkeley.edu>
2025-01-03 14:20:14 -0800

    Revert "Primitive implementation working."
    This reverts commit 126f2f3377e3e97921029b57c644fd390b36d66e.

--- src/implementations/warp_matmul.py
+++ src/implementations/warp_matmul.py
@@ -3,25 +3,17 @@ from build.kernel_wrapper import *
 from src.templates.jinja_utils import *
 
 class WarpMatmulTest:
-    def __init__(self, M, N, K, dtype_in, dtype_accum, prng_seed=12345):
+    def __init__(self, M, N, K, dtype, prng_seed=12345):
         self.M, self.N, self.K = M, N, K
         rng = np.random.default_rng(12345)
-        self.A = np.array(rng.random((M, K), dtype=np.float32).astype(dtype_in))
-        self.B = rng.random((K, N), dtype=np.float32).astype(dtype_in)
-
-        self.A[:] = 1.0
-        self.B[:] = 1.0
-
-        self.A[1, 0] = 2.0
-        self.B[1, 0] = 2.0
-        self.C = (self.A @ self.B).astype(dtype_accum)
+        self.A = rng.random((M, K), dtype=dtype)
+        self.B = rng.random((K, N), dtype=dtype)
+        self.C = self.A @ self.B 
 
     def run(self, kernel):
         cpp_tester = MMTester(kernel)
 
-        A_test = self.A.T.copy()
-
-        a_dev = DeviceBuffer(A_test)
+        a_dev = DeviceBuffer(self.A)
         b_dev = DeviceBuffer(self.B)
 
         wmm_result = np.zeros((self.M, self.N), dtype=self.C.dtype)
@@ -30,24 +22,14 @@ class WarpMatmulTest:
 
         wmm_dev.copy_to_host()
 
-        # Try every combo of A.T, B.T, and C.T to see which matmul matches
-        # the result of the kernel
-
-        print("#1?", np.allclose(wmm_result, (self.A @ self.B).astype(np.float32)))
-        print("#2?", np.allclose(wmm_result, (self.A.T @ self.B).astype(np.float32)))
-        print("#3?", np.allclose(wmm_result, (self.A @ self.B.T).astype(np.float32)))
-        print("#4?", np.allclose(wmm_result, (self.A.T @ self.B.T).astype(np.float32)))
-        print("#5?", np.allclose(wmm_result, (self.A @ self.B).astype(np.float32).T))
-        print("#6?", np.allclose(wmm_result, (self.A.T @ self.B).astype(np.float32).T))
-        print("#7?", np.allclose(wmm_result, (self.A @ self.B.T).astype(np.float32).T))
-        print("#8?", np.allclose(wmm_result, (self.A.T @ self.B.T).astype(np.float32).T))
+        print("WarpMatmul pass?", np.allclose(wmm_result, self.C))
 
 def test_simple_kernel():
     env = get_jinja_environment()
     template = env.get_template("wmm.cuh")
     env.globals['enumerate'] = enumerate
 
-    M, N, K = 64, 64, 64
+    M, N, K = 32, 32, 16
     kernel = template.render(M=M, N=N, K=K) 
-    test = WarpMatmulTest(M, N, K, np.float16, np.float32)
+    test = WarpMatmulTest(M, N, K, np.float32)
     test.run(kernel)
--- src/templates/wmm.cuh
+++ src/templates/wmm.cuh
@@ -1,98 +1,15 @@
-#include "cutlass/gemm/warp/default_mma_tensor_op.h"
+__global__ void warp_matmul(float* A, float* B, float* C) {    
+    int t_idx = threadIdx.x + blockIdx.x * blockDim.x;
 
-#define ROW_OPERATION(ROW_LEN, LOOP_VAR, ...) \
-    _Pragma ("unroll") \
-    for(int LOOP_VAR = 0; LOOP_VAR < ROW_LEN; LOOP_VAR += THREADS_PER_WARP) { \
-        if(LOOP_VAR >= ROW_LEN - THREADS_PER_WARP) { \
-            if(lane_id < ROW_LEN - LOOP_VAR) { \
-                __VA_ARGS__  \
-            } \
-        } \
-        else { \
-            __VA_ARGS__  \
-        } \
-    }
-
-
-using LayoutA = cutlass::layout::ColumnMajorTensorOpMultiplicandCongruous<
-    cutlass::sizeof_bits<cutlass::half_t>::value, 64>;
-
-using LayoutB = cutlass::layout::RowMajorTensorOpMultiplicandCongruous<
-    cutlass::sizeof_bits<cutlass::half_t>::value, 64>;
-
-using WarpMma = typename cutlass::gemm::warp::DefaultMmaTensorOp<
-    cutlass::gemm::GemmShape<64, 64, 8>,                            // Overall warp-level GEMM operation
-    cutlass::gemm::GemmShape<16, 8, 8>,                             // Target instruction
-    cutlass::half_t, LayoutA,                                       // operand A type and layout
-    cutlass::half_t, LayoutB,                                       // operand B type and layout
-    float,                                                          // accumulator type
-    cutlass::layout::RowMajor>::Type;                               // accumulator layout
-
-__global__ void warp_matmul(cutlass::half_t *A, cutlass::half_t *B, float* C) {
-    int const kGemmK = 64;
-    int lane_id = threadIdx.x;
-    int THREADS_PER_WARP = 32;
-
-    __shared__ cutlass::half_t smem_buffer_A[WarpMma::Shape::kM * kGemmK];
-    __shared__ cutlass::half_t smem_buffer_B[WarpMma::Shape::kN * kGemmK];
-    __shared__ float smem_buffer_C[WarpMma::Shape::kM * WarpMma::Shape::kN];
-
-    ROW_OPERATION(WarpMma::Shape::kM * kGemmK, i, smem_buffer_A[i + lane_id] = A[i + lane_id];)
-    ROW_OPERATION(WarpMma::Shape::kN * kGemmK, i, smem_buffer_B[i + lane_id] = B[i + lane_id];)
-
-    __syncwarp();
-
-    //int t_idx = threadIdx.x + blockIdx.x * blockDim.x;
-
-    int lda = WarpMma::Shape::kM;
-    int ldb = WarpMma::Shape::kN;
-    int ldc = WarpMma::Shape::kN;
-
-    WarpMma::IteratorA iter_A({smem_buffer_A, lda}, lane_id);
-    WarpMma::IteratorB iter_B({smem_buffer_B, ldb}, lane_id);
-    WarpMma::IteratorC iter_C({smem_buffer_C, ldc}, lane_id);
-
-    WarpMma::FragmentA frag_A;
-    WarpMma::FragmentB frag_B;
-    WarpMma::FragmentC accum;
-
-    WarpMma mma;
-    accum.clear();
-
-    #pragma unroll 1
-    for (int k = 0; k < kGemmK; k += WarpMma::Shape::kK) { 
-        iter_A.load(frag_A);  // Load fragments from A and B matrices
-        iter_B.load(frag_B);
-
-        ++iter_A; ++iter_B;   // Advance along GEMM K to next tile in A
-                                //   and B matrices
-
-                                // Compute matrix product
-        mma(accum, frag_A, frag_B, accum);
-    }
-
-    __syncwarp();
-    iter_C.store(accum);
-
-    __syncwarp();
-    ROW_OPERATION(WarpMma::Shape::kM * WarpMma::Shape::kN, i, C[i + lane_id] = smem_buffer_C[i + lane_id];)
-
-}
-
-/* Old implementation 
-
-int t_idx = threadIdx.x + blockIdx.x * blockDim.x;
-
-if(t_idx == 0) {
-    for(int i = 0; i < {{M}}; i++) {
-        for(int j = 0; j < {{N}}; j++) {
-            float sum = 0;
-            for(int k = 0; k < {{K}}; k++) {
-                sum += A[i * {{K}} + k] * B[k * {{N}} + j];
+    if(t_idx == 0) {
+        for(int i = 0; i < {{M}}; i++) {
+            for(int j = 0; j < {{N}}; j++) {
+                float sum = 0;
+                for(int k = 0; k < {{K}}; k++) {
+                    sum += A[i * {{K}} + k] * B[k * {{N}} + j];
+                }
+                C[i * {{N}} + j] = sum;
             }
-            C[i * {{N}} + j] = sum;
         }
     }
 }
-
-*/
