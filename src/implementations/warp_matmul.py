0170a8bc7b539a546bd36e8f6ad6941634ca0941
Vivek Bharadwaj <vivek_bharadwaj@berkeley.edu>
2025-01-03 18:02:59 -0800

    Kernel generator complete.

--- src/implementations/warp_matmul.py
+++ src/implementations/warp_matmul.py
@@ -9,8 +9,7 @@ class WarpMatmulTest:
         self.A = rng.random((M, K), dtype=dtype)
         self.B = rng.random((K, N), dtype=dtype)
 
-        self.A = np.array(self.A, order='C')
-
+        self.A = np.array(self.A, order='F')
         self.C = self.A @ self.B 
 
     def run(self, kernel):
@@ -19,20 +18,24 @@ class WarpMatmulTest:
         a_dev = DeviceBuffer(self.A)
         b_dev = DeviceBuffer(self.B)
 
-        wmm_result = np.zeros((self.M, self.N), dtype=self.C.dtype)
+        wmm_result = np.zeros((self.M, self.N), dtype=self.C.dtype, order='F')
         wmm_dev = DeviceBuffer(wmm_result)
         cpp_tester.execute(a_dev.data_ptr(), b_dev.data_ptr(), wmm_dev.data_ptr())
 
         wmm_dev.copy_to_host()
 
         print("WarpMatmul pass?", np.allclose(wmm_result, self.C))
+        print(wmm_result)
+        print(self.C)
+
 
 def test_simple_kernel():
     env = get_jinja_environment()
     template = env.get_template("wmm.cuh")
     env.globals['enumerate'] = enumerate
 
-    M, N, K = 32, 32, 16
-    kernel = template.render(M=M, N=N, K=K) 
+    M, N, K = 17, 3, 5 
+    kernel = template.render(M=M, N=N, K=K, 
+            TILES_PER_ROW=4, STOREBACK_RMAJOR=False)
     test = WarpMatmulTest(M, N, K, np.float32)
     test.run(kernel)
--- src/templates/wmm.cuh
+++ src/templates/wmm.cuh
@@ -1,15 +1,83 @@
+{%-set TILES_PER_COL = 32 // TILES_PER_ROW %}
+
 __global__ void warp_matmul(float* A, float* B, float* C) {    
     int t_idx = threadIdx.x + blockIdx.x * blockDim.x;
+    int lane_id = t_idx % 32;
 
-    if(t_idx == 0) {
-        for(int i = 0; i < {{M}}; i++) {
-            for(int j = 0; j < {{N}}; j++) {
-                float sum = 0;
-                for(int k = 0; k < {{K}}; k++) {
-                    sum += A[i * {{K}} + k] * B[k * {{N}} + j];
+    int const rpt = {{(M + TILES_PER_COL - 1) // TILES_PER_COL}};
+    int const cpt = {{(N + TILES_PER_ROW - 1) // TILES_PER_ROW}};
+
+    float row[cpt];
+    float col[rpt];
+    float tile[rpt][cpt];
+
+    int TI_idx = lane_id / {{TILES_PER_ROW}}; 
+    int TJ_idx = lane_id % {{TILES_PER_ROW}};
+    int is = TI_idx * rpt; int ie = (TI_idx + 1) * rpt;
+    int js = TJ_idx * cpt; int je = (TJ_idx + 1) * cpt;
+    int ist = min(is, {{M}}); int iet = min(ie, {{M}});
+    int jst = min(js, {{N}}); int jet = min(je, {{N}});
+
+    // Zero the output tile
+    #pragma unroll
+    for(int i = 0; i < rpt; i++) {
+        #pragma unroll
+        for(int j = 0; j < cpt; j++) {
+            tile[i][j] = 0.0f;
+        }
+    }
+
+    for(int k = 0; k < {{K}}; k++) {
+        #pragma unroll
+        for(int i = 0; i < rpt; i++) {
+            if(ist + i < {{M}}) {
+                col[i] = A[k * {{M}} + ist + i];
+            }
+        }
+
+        #pragma unroll
+        for(int j = 0; j < cpt; j++) {
+            if(jst + j < {{N}}) {
+                row[j] = B[k * {{N}} + jst + j];
+            }
+        }
+
+        #pragma unroll
+        for(int i = 0; i < rpt; i++) {
+            #pragma unroll
+            for(int j = 0; j < cpt; j++) {
+                if(ist + i < {{M}} && jst + j < {{N}}) {
+                    tile[i][j] += col[i] * row[j];
                 }
-                C[i * {{N}} + j] = sum;
             }
         }
     }
+
+    // Store the output
+    #pragma unroll
+    for(int i = 0; i < rpt; i++) {
+        for(int j = 0; j < cpt; j++) {
+            if(i + ist < {{M}} && j + jst < {{N}}) {
+                {%- if STOREBACK_RMAJOR %}
+                    C[(i + ist) * {{N}} + j + jst] = tile[i][j];
+                {%- else %}
+                    C[(j + jst) * {{M}} + i + ist] = tile[i][j];
+                {%- endif %}
+            }
+        } 
+    }
+    // Print lane_id, is, ie, js, je
+    // printf("lane_id: %d, is: %d, ie: %d, js: %d, je: %d\n", lane_id, is, ie, js, je);
 }
+
+/*if(t_idx == 0) {
+    for(int i = 0; i < {{M}}; i++) {
+        for(int j = 0; j < {{N}}; j++) {
+            float sum = 0;
+            for(int k = 0; k < {{K}}; k++) {
+                sum += A[i * {{K}} + k] * B[k * {{N}} + j];
+            }
+            C[i * {{N}} + j] = sum;
+        }
+    }
+}*/
