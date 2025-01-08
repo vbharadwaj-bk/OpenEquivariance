713fad0dbfd51f776162feb589939c396feccf50
Austin Glover <austin_glover@berkeley.com>
2025-01-07 21:54:55 -0800

    paper benchmarks now tetris

--- src/benchmark/benchmark_routines.py/paper_benchmark_uvw.py
+++ src/benchmark/benchmark_routines.py/paper_benchmark_uvw.py
@@ -10,35 +10,43 @@ from src.implementations.E3NNTensorProduct import E3NNTensorProduct
 from src.implementations.LoopUnrollTP import LoopUnrollTP
 from src.implementations.CUETensorProduct import CUETensorProduct
 from src.implementations.MultiplicityOuterProductTP import MultiplicityOuterProductTP
+from src.implementations.LoopReorderUVWTP import LoopReorderUVWTP
 from src.benchmark.TestBenchmarkSuite import TestBenchmarkSuite, TestDefinition, Direction
 from src.benchmark.tpp_creation_utils import FullyConnectedTPProblem
+from src.benchmark.benchmark_configs import e3nn_torch_tetris, e3nn_docs_tetris
 
 logger = getLogger()
 
 if __name__ == '__main__':
     FCTPP = FullyConnectedTPProblem
 
-    problems = [
-            FCTPP("2x1e", "2x1e", "2x1e"),
-            FCTPP("2x4e", "2x4e", "2x4e"),
-            FCTPP("2x8e", "2x8e", "2x8e"),
+    # problems = [
+    #         # FCTPP("2x1e", "2x1e", "2x1e"),
+    #         # FCTPP("2x4e", "2x4e", "2x4e"),
+    #         # FCTPP("2x8e", "2x8e", "2x8e"),
 
-            FCTPP("4x1e", "4x1e", "4x1e"),
-            FCTPP("4x4e", "4x4e", "4x4e"),
-            FCTPP("4x8e", "4x8e", "4x8e"),
+    #         # FCTPP("4x1e", "4x1e", "4x1e"),
+    #         # FCTPP("4x4e", "4x4e", "4x4e"),
+    #         # FCTPP("4x8e", "4x8e", "4x8e"),
 
-            FCTPP("8x1e", "8x1e", "8x1e"),
-            FCTPP("8x4e", "8x4e", "8x4e"),
-            FCTPP("8x8e", "8x8e", "8x8e"),
+    #         # FCTPP("8x1e", "8x1e", "8x1e"),
+    #         # FCTPP("8x4e", "8x4e", "8x4e"),
+    #         # FCTPP("8x8e", "8x8e", "8x8e"),
 
-            FCTPP("16x1e", "16x1e", "16x1e"),
-            FCTPP("16x4e", "16x4e", "16x4e"),
-            FCTPP("16x8e", "16x8e", "16x8e"),
+    #         # FCTPP("16x1e", "16x1e", "16x1e"),
+    #         # FCTPP("16x4e", "16x4e", "16x4e"),
+    #         # FCTPP("16x8e", "16x8e", "16x8e"),
 
-            FCTPP("32x1e", "32x1e", "32x1e"),
-            FCTPP("32x4e", "32x4e", "32x4e"),
-            # FCTPP("32x8e", "32x8e", "32x8e"),
-        ]
+    #         FCTPP("32x1e", "32x1e", "32x1e"),
+    #         FCTPP("32x4e", "32x4e", "32x4e"),
+    #         # FCTPP("32x8e", "32x8e", "32x8e"),
+
+    #         FCTPP("8x1e + 8x1e + 8x1e + 8x1e", "31x1e", "32x1e"),
+    #         FCTPP("8x4e + 8x4e + 8x4e + 8x4e", "31x4e", "32x4e"),
+    #         # FCTPP("8x8e + 8x8e + 8x8e + 8x8e", "31x8e", "32x8e"),
+    #     ]
+
+    problems = e3nn_docs_tetris
 
     directions : list[Direction] = [
         'forward',
@@ -48,7 +56,8 @@ if __name__ == '__main__':
     implementations = [
             E3NNTensorProduct,
             # CUETensorProduct, 
-            MultiplicityOuterProductTP
+            MultiplicityOuterProductTP,
+            # LoopReorderUVWTP,
         ]
 
     tests = [TestDefinition(implementation, problem, direction, correctness=False, benchmark=True) 
