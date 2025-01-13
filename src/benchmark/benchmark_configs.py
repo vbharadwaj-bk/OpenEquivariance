e8da917fe7f52b43e2fd51ee15d9b288f12100ca
Austin Glover <austin_glover@berkeley.com>
2025-01-13 12:22:34 -0800

    better documentation of source

--- src/benchmark/benchmark_configs.py
+++ src/benchmark/benchmark_configs.py
@@ -1,5 +1,7 @@
 from src.benchmark.tpp_creation_utils import FullyConnectedTPProblem as FCTPP
 
+# source: https://github.com/e3nn/e3nn/blob/main/examples/tetris.py
+# running tetris will output the layers. I've only extracted the fully connected layers here. 
 e3nn_torch_tetris = [
     # 0th Layer 
     FCTPP("1x0e", "1x0e", "150x0e + 50x1o + 50x2e"), #sc 
@@ -10,7 +12,7 @@ e3nn_torch_tetris = [
     # 1st Layer
     FCTPP("50x0e + 50x1o + 50x2e", "1x0e", "250x0e + 50x1o + 50x1e + 50x2o + 50x2e"), #sc
     FCTPP("50x0e + 50x1o + 50x2e", "1x0e", "50x0e + 50x1o + 50x2e"), #lin1 
-    FCTPP("50x0e + 50x1o + 50x2e", "1x0e + 1x1o + 1x2e",  "150x0e + 200x1o + 100x1e + 100x2o + 200x2e"), #tp
+    # FCTPP("50x0e + 50x1o + 50x2e", "1x0e + 1x1o + 1x2e",  "150x0e + 200x1o + 100x1e + 100x2o + 200x2e"), #tp
     FCTPP("150x0e + 200x1o + 100x1e + 100x2o + 200x2e", "1x0e", "250x0e + 50x1o + 50x1e + 50x2o + 50x2e"), #lin2
     FCTPP("150x0e + 200x1o + 100x1e + 100x2o + 200x2e", "1x0e", "1x0e"), #alpha
 
@@ -31,7 +33,16 @@ e3nn_torch_tetris = [
 # jax version can be found here, but doesn't directly translate 
 # https://github.com/e3nn/e3nn-jax/blob/main/examples/tetris_point.py
 
-e3nn_docs_tetris = [
-    FCTPP("1x0e + 1x1o + 1x2e + 1x3o", "1x0e + 1x1o + 1x2e + 1x3o", "64x0e + 24x1e + 24x1o + 16x2e + 16x2o"), #tp1 
-    FCTPP("64x0e + 24x1e + 24x1o + 16x2e + 16x2o", "1x0e + 1x1o + 1x2e + 1x3o", "0o + 6x0e"), #tp2 
+
+# source: https://github.com/e3nn/e3nn/blob/f95297952303347a8a3cfe971efe449c710c43b2/examples/tetris_polynomial.py#L66-L68
+e3nn_tetris_polynomial = [
+    FCTPP("1x0e + 1x1o + 1x2e + 1x3o", "1x0e + 1x1o + 1x2e + 1x3o", "64x0e + 24x1e + 24x1o + 16x2e + 16x2o", label="e3nn tetris polynomial first layer"), #tp1 
+    FCTPP("64x0e + 24x1e + 24x1o + 16x2e + 16x2o", "1x0e + 1x1o + 1x2e + 1x3o", "0o + 6x0e", label="e3nn tetris polynomial second layer"), #tp2 
+]
+
+# https://github.com/gcorso/DiffDock/blob/b4704d94de74d8cb2acbe7ec84ad234c09e78009/models/tensor_layers.py#L299
+# specific irreps come from vivek's communication with diff dock team
+diffdock_configs = [
+    FCTPP("10x1o + 10x1e + 48x0e + 48x0o", "1x0e + 1x1o", "10x1o + 10x1e + 48x0e + 48x0o", shared_weights=False, label='diffdock spherical harmonics (1)'),
+    FCTPP("10x1o + 10x1e + 48x0e + 48x0o", "1x0e + 1x1o + 1x2e", "10x1o + 10x1e + 48x0e + 48x0o", shared_weights=False, label='diffdock spherical harmonics (2)'),
 ]
