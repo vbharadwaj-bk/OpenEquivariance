b9f393eb48eca7a62ea529432f3a99b0fdbf80d1
Vivek Bharadwaj <vivek_bharadwaj@berkeley.edu>
2025-01-12 18:07:50 -0800

    Minor modifications to CMakeLists.txt, removed env.sh.

--- env.sh
+++
@@ -1,9 +0,0 @@
-module load conda 
-conda activate equivariant_spmm_env
-module load cudatoolkit
-
-#module load python
-
-# dcgmi profile --pause
-# ncu -c 2 -o report.ncu-rep python driver.py
-# dcgmi profile --resume 
