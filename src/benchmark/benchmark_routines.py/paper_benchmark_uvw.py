556df86f7364a7ce8286b74c2767ee2b34a0f7e5
Austin Glover <austin_glover@berkeley.com>
2025-01-14 14:00:26 -0800

    working towards benchmarking

--- src/benchmark/benchmark_routines.py/paper_benchmark_uvw.py
+++ src/benchmark/benchmark_routines.py/paper_benchmark_uvw.py
@@ -1,19 +1,18 @@
-
 import itertools
-import sys, os
+import sys
+import os
+import logging
 
 sys.path.insert(1, os.path.join(sys.path[0], '../../../'))
 
-from src.benchmark.logging_utils import *
+from src.benchmark.logging_utils import getLogger
 
-from src.implementations.E3NNTensorProduct import E3NNTensorProduct 
-from src.implementations.ManyOneUVWTP import ManyOneUVWTP
+from src.implementations.E3NNTensorProduct import E3NNTensorProductCompiledMaxAutotuneCUDAGraphs
 from src.implementations.CUETensorProduct import CUETensorProduct
-from src.implementations.MultiplicityOuterProductTP import MultiplicityOuterProductTP
-from src.implementations.LoopReorderUVWTP import LoopReorderUVWTP
+from src.implementations.LoopUnrollTP import LoopUnrollTP
 from src.benchmark.TestBenchmarkSuite import TestBenchmarkSuite, TestDefinition, Direction
 from src.benchmark.tpp_creation_utils import FullyConnectedTPProblem
-from src.benchmark.benchmark_configs import e3nn_torch_tetris, e3nn_torch_tetris_polynomial
+from src.benchmark.benchmark_configs import e3nn_torch_tetris_polynomial, diffdock_configs
 
 logger = getLogger()
 
@@ -22,6 +21,7 @@ if __name__ == '__main__':
 
     problems =  list(itertools.chain(
         e3nn_torch_tetris_polynomial,
+        diffdock_configs,
     ))
 
     directions : list[Direction] = [
@@ -30,10 +30,9 @@ if __name__ == '__main__':
     ]
 
     implementations = [
-            E3NNTensorProduct,
-            # CUETensorProduct, 
-            MultiplicityOuterProductTP,
-            # LoopReorderUVWTP,
+            E3NNTensorProductCompiledMaxAutotuneCUDAGraphs,
+            CUETensorProduct, 
+            LoopUnrollTP,
         ]
 
     tests = [TestDefinition(implementation, problem, direction, correctness=False, benchmark=True) 
