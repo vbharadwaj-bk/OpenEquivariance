e5997b2d9387bfd59c91178ea9bf32487a61cf82
Austin Glover <austin_glover@berkeley.com>
2025-01-23 15:08:09 -0800

    remove mathdx references

--- io/set_mathdx_env_vars.sh
+++
@@ -1,7 +0,0 @@
-# These env vars came from: 
-# https://docs.nersc.gov/development/build-tools/cmake/#linking-cuda-math-libraries
-
-module load cudatoolkit
-
-export CMAKE_PREFIX_PATH=${CUDA_HOME}/../../math_libs:${CMAKE_PREFIX_PATH}
-export CPATH=${CUDA_HOME}/../../math_libs/include:${CPATH}
